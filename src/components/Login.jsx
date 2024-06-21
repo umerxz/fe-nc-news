c6c290fecd4cd665259d23cca5e7633a2b7a8bec
Umer Naseer <umernaseer02@gmail.com>
2024-06-21 13:34:20 +0100

    add loading spinner for components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,47 +1,59 @@
 import { useState, useContext } from 'react'
 import { UserContext } from '../context/UserProvider'
 import { Link, useNavigate } from 'react-router-dom'
+import { LoadingSpinner } from './LoadingSpinner';
 
 // eslint-disable-next-line react/prop-types
 const Login = ({accountCreated}) => {
     const [username, setUsername] = useState('')
     const [errorMsg, setErrorMsg] = useState('')
+    const [loading,setLoading] = useState(false)
     const { loginUser } = useContext(UserContext)
     const navigate = useNavigate()
 
     const handleLoginSubmit = (event) => {
         event.preventDefault()
+        setLoading(true)
         setErrorMsg('')
         loginUser(username)
         .then(() => {
+            setLoading(false)
             navigate('/articles')
         })
         .catch((err) => {
+            setLoading(false)
             setErrorMsg(err.data.msg)
         })
     }
 
     return (
-        <div className='formbox'>
-            <form onSubmit={handleLoginSubmit}>
-                <h1 className='formHeading'>Login</h1>
-                {accountCreated? <p>Login to the Account just created</p> :null}
-                <label>Enter your username:
-                    <input
-                        required
-                        type="text"
-                        name="username"
-                        value={username || ""}
-                        onChange={(event) => setUsername(event.target.value)}
-                    />
-                </label>
-                <br></br>
-                <p style={{color:'red'}}>{errorMsg?errorMsg:null}</p>
-                <button className='btns' type='submit'>Login</button>
-            </form>
-            <Link style={{textAlign:'center',color:'lightgrey'}} to='/signup'><p className='switchForm'>Don&apos;t Have An Account? Click Here To SignUp!</p></Link>
-
-        </div>
+        <>
+            {
+                loading
+                ?
+                <LoadingSpinner/>
+                :
+                <div className='formbox'>
+                    <form onSubmit={handleLoginSubmit}>
+                        <h1 className='formHeading'>Login</h1>
+                        {accountCreated? <p>Login to the Account just created</p> :null}
+                        <label>Enter your username:
+                            <input
+                                required
+                                type="text"
+                                name="username"
+                                value={username || ""}
+                                onChange={(event) => setUsername(event.target.value)}
+                            />
+                        </label>
+                        <br></br>
+                        <p style={{color:'red'}}>{errorMsg?errorMsg:null}</p>
+                        <button className='btns' type='submit'>Login</button>
+                    </form>
+                    <Link style={{textAlign:'center',color:'lightgrey'}} to='/signup'><p className='switchForm'>Don&apos;t Have An Account? Click Here To SignUp!</p></Link>
+                </div>
+            }
+        </>
     );
 };
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { postUser } from '../api/api';
+import { LoadingSpinner } from './LoadingSpinner';
 
 // eslint-disable-next-line react/prop-types
 const Signup = ({setAccountCreated}) => {
@@ -8,15 +9,17 @@ const Signup = ({setAccountCreated}) => {
     const [name, setName] = useState('')
     const [avatarUrl, setAvatarUrl] = useState('')
     const [usernameErrorMsg, setUsernameErrorMsg] = useState('')
+    const [loading,setLoading] = useState(false)
     const [avatarErrorMsg, setAvatarErrorMsg] = useState('')
     const navigate = useNavigate();
 
     const handleSignupSubmit = (event) => {
+        setLoading(true)
         event.preventDefault()
         const urlRegex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i
         const isValid = urlRegex.test(avatarUrl)
         if(isValid===false){
-            setUsernameErrorMsg('')
+            setLoading(false)
             setAvatarErrorMsg('Invalid Avatar Url!')
         }
         else{
@@ -24,10 +27,12 @@ const Signup = ({setAccountCreated}) => {
             setAvatarErrorMsg('')
             postUser({ username, name, avatarUrl })
             .then(() => {
+                setLoading(false)
                 setAccountCreated(true)
                 navigate('/login') 
             })
             .catch((err) => {
+                setLoading(false)
                 setAccountCreated(false)
                 setUsernameErrorMsg(err.data.msg)
             })
@@ -35,44 +40,52 @@ const Signup = ({setAccountCreated}) => {
     }
 
     return (
-        <div className='formbox'>
-            <form onSubmit={handleSignupSubmit}>
-                <h1 className='formHeading'>SignUp</h1>
-                <label>Enter your username:
-                    <input
-                        required
-                        type="text"
-                        name="username"
-                        value={username || ""}
-                        onChange={(event) => setUsername(event.target.value)}
-                    />
-                </label>
-                {usernameErrorMsg?<p style={{color:'red'}}>{usernameErrorMsg}</p>:null}
-                <p>Length: 6-20. Must have letters. Can have 2 symbols - . numbers. Cannot end with symbol </p>
-                <label>Enter your name:
-                    <input
-                        required
-                        type="text"
-                        name="name"
-                        value={name || ""}
-                        onChange={(event) => setName(event.target.value)}
-                    />
-                </label>
-                <br></br>
-                <label>Enter your image avatar:
-                    <input
-                        required
-                        type="text"
-                        name="avatar_url"
-                        value={avatarUrl || ""}
-                        onChange={(event) => setAvatarUrl(event.target.value)}
-                    />
-                </label>
-                {avatarErrorMsg?<p style={{color:'red'}}>{avatarErrorMsg}</p>:null}
-                <button className='btns' type='submit'>Sign up</button>
-            </form>
-            <Link style={{textAlign:'center',color:'lightgrey'}} to='/login'><p className='switchForm'>Don&apos;t Have An Account? Click Here To SignUp!</p></Link>
-        </div>
+        <>
+            {
+                loading
+                ?
+                <LoadingSpinner/>
+                :
+                <div className='formbox'>
+                    <form onSubmit={handleSignupSubmit}>
+                        <h1 className='formHeading'>SignUp</h1>
+                        <label>Enter your username:
+                            <input
+                                required
+                                type="text"
+                                name="username"
+                                value={username || ""}
+                                onChange={(event) => setUsername(event.target.value)}
+                            />
+                        </label>
+                        {usernameErrorMsg?<p style={{color:'red'}}>{usernameErrorMsg}</p>:null}
+                        <p>Length: 6-20. Must have letters. Can have 2 symbols - . numbers. Cannot end with symbol </p>
+                        <label>Enter your name:
+                            <input
+                                required
+                                type="text"
+                                name="name"
+                                value={name || ""}
+                                onChange={(event) => setName(event.target.value)}
+                            />
+                        </label>
+                        <br></br>
+                        <label>Enter your image avatar:
+                            <input
+                                required
+                                type="text"
+                                name="avatar_url"
+                                value={avatarUrl || ""}
+                                onChange={(event) => setAvatarUrl(event.target.value)}
+                            />
+                        </label>
+                        {avatarErrorMsg?<p style={{color:'red'}}>{avatarErrorMsg}</p>:null}
+                        <button className='btns' type='submit'>Sign up</button>
+                    </form>
+                    <Link style={{textAlign:'center',color:'lightgrey'}} to='/login'><p className='switchForm'>Don&apos;t Have An Account? Click Here To SignUp!</p></Link>
+                </div>
+            }
+        </>
     );
 };
 
